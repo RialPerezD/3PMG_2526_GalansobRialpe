6852b68a1972be1b382af15816475d7642ff118f
galansob <galansob@esat-libertad.com>
2025-11-25 20:25:56 +0100

    Adding coments WIP

--- include/MotArda/JobSystem.hpp
+++ include/MotArda/JobSystem.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file JobSystem.h
+ * @brief JobSystem (multi-threads) implementation
+ * @author Iván Galán <galansob@esat-alumni.com>
+ */
+
+
 #pragma once
 
 #include <vector>
@@ -9,16 +16,41 @@
 #include <iostream>
 
 namespace MTRD {
-
+    /**
+    * @class JobSystem
+    * @brief A class that will manage the threads 
+    * @details This class will manage the multiple threads and mutex,
+    * being able to queue tasks to them.
+    */
     class JobSystem {
-  
+    
     public:
+        /**
+        * @brief Constructor
+        * @details Initializes the amount of threads and then places them
+        * at the back.
+        */
         JobSystem();
+        /**
+        * @brief Destructor
+        * @details Turns the "stop_" boolean to true and notify all threads, 
+        * freeing the threads. 
+        */
         ~JobSystem();
-
+        
+        /**
+        * @brief Copy constructor
+        * @param JobSystem& right Reference to a JobSystem type object
+        * @details We set it to delete because there is no need to copy anything.
+        */
         JobSystem(const JobSystem& right) = delete;
         JobSystem& operator=(const JobSystem& right) = delete;
 
+        /**
+        * @brief Move constructor
+        * @param JobSystem&& right Reference to a JobSystem type object
+        * @details We set it to delete because there is no need to copy anything.
+        */
         JobSystem(JobSystem&& right) = default;
         JobSystem& operator=(JobSystem&& right) = default;
 
