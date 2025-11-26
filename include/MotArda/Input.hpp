024f83bec12f03354a2947d3a93abc551725ec0d
galansob <galansob@esat-libertad.com>
2025-11-26 20:23:51 +0100

    More doxygen comments

--- include/MotArda/Input.hpp
+++ include/MotArda/Input.hpp
@@ -8,6 +8,10 @@
 
 namespace MTRD {
 
+    /**
+    * @class Input
+    * @brief Manages the inputs of the engine.
+    */
 	class Input {
 	public:
         enum class Keyboard {
@@ -38,31 +42,73 @@ namespace MTRD {
             Y,
             Z
         };
-
+        
+        /**
+        * @brief Creates an Input type object.
+        * @param Window& window Reference to a Window type object.
+        * @details Register a new Input.
+        */
 		static Input inputCreate(Window& window);
 
-        //TODO
+        /**
+        * @brief Destructor
+        * @details 
+        */
 		~Input();
 
 		//--Dissable copy constructor and enable noving constructors--
+
+        /**
+        * @brief Copy constructor.
+        * @param Input& right Reference to an Input type object.
+        * @details We set it to delete because there is no need to copy anything.
+        */
 		Input(const Input& right) = delete;
+        /**
+        * @brief Copy assignment.
+        * @param Input& right Reference to an Input type object.
+        * @details We set it to delete because there is no need to copy anything.
+        */
 		Input& operator=(const Input& right) = delete;
 
+        /**
+        * @brief Move constructor.
+        * @param Input&& right Reference to an Input type object.
+        * @details --------------
+        */
 		Input(Input&& right);
 		Input& operator=(Input&& right) = default;
 
 		//------------Functions-----------------------
 
+        /**
+        * @brief Calls the setKeyCallback function.
+        * @param Keyboard The engine will check if the 
+        * key matches with user's pressed one.
+        * @details ------------
+        */
 		bool isKeyPressed(Keyboard);
         bool isKeyDown(Keyboard);
         bool isKeyUp(Keyboard);
 
         void generateAsciiMap();
+        /**
+        * @brief Calls the setKeyCallback function.
+        * @param Window& window Reference to a Window type object.
+        * @details It allows the input events to happen.
+        */
         void setKeyboardCallback(Window& window);
+        /**
+        * @brief Release the vectors.
+        * @details Clears the pressKey and releaseKey vectors.
+        */
         void clearBuffers();
 
+        //< Key pressed by the user.
         static std::vector<int> pressKey;
+        //< Key being holded by the user.
 		static std::vector<int> repeatKey;
+        //< Key released this frame by the user.
         static std::vector<int> releaseKey;
 
 
--- include/MotArda/JobSystem.hpp
+++ include/MotArda/JobSystem.hpp
@@ -44,31 +44,69 @@ namespace MTRD {
         * @details We set it to delete because there is no need to copy anything.
         */
         JobSystem(const JobSystem& right) = delete;
+        /**
+        * @brief Copy assignment
+        * @param JobSystem& right Reference to a JobSystem type object
+        * @details We set it to delete because there is no need to copy anything.
+        */
         JobSystem& operator=(const JobSystem& right) = delete;
 
         /**
         * @brief Move constructor
         * @param JobSystem&& right Reference to a JobSystem type object
-        * @details We set it to delete because there is no need to copy anything.
+        * @details We are able to move threads as unique_ptr allows it.
         */
         JobSystem(JobSystem&& right) = default;
+        /**
+        * @brief Move assignment
+        * @param JobSystem&& right Reference to a JobSystem type object
+        * @details We are able to move threads as unique_ptr allows it.
+        */
         JobSystem& operator=(JobSystem&& right) = default;
 
+        /** 
+        * @brief Adds a task to the queue
+        * @param std::function<void()> task The function we will we adding to the queue
+        * @details Adds a task to the queue by blocking the mutex and waking up
+        * a thread.
+        */
         void enqueue(std::function<void()> task);
 
 
     private:
+        /**
+         * @struct JobSystemData
+         * @brief Internal data structure used by JobSystem.
+         * @details Stores the worker threads, the task queue, synchronization
+         * primitives and state used by the JobSystem thread pool.
+         * Additionally, we make the struct's object as unique_ptr so
+         * we can move or copy the content.
+         */
         struct JobSystemData {
+            //< Vector that contains the worker threads.
             std::vector<std::thread> workers_;
+            //< Queue of future functions to be processed by workers.
             std::queue<std::function<void()>> tasks_;
 
+            //< Mutex prevents shared data from being simultaneously 
+            //< accessed by multiple threads.
             std::mutex queue_mutex_;
+
             std::condition_variable condition_;
+            //< The amount of threads the JobSystem will manage.
             size_t threads;
 
+            //< Boolean variable that allow workers to free.
             std::atomic<bool> stop_;
         };
 
+        /**
+        * @brief Manage the threads behavior
+        * @param JobSystemData* data --------------------
+        * @details Creates an infinite loop until "stop_" is true or the function
+        * finds a task. 
+        * The function locks the mutex to be able to check the queue
+        */
         static void worker(JobSystemData* data);
 
         std::unique_ptr<JobSystemData> data_;
