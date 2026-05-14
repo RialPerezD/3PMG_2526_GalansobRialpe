2d6b5e4cf49c0a45626d328df34e459c815af952
RialPerezD <rialpe@esat-alumni.com>
2026-05-14 14:13:31 +0200

    default premake

---
+++ include/win64/MotArda/Input.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <MotArda/window.hpp>
+
+#include <optional>
+#include <vector>
+#include <map>
+
+
+namespace MTRD {
+
+    /**
+    * @class Input
+    * @brief Manages the inputs of the engine.
+    */
+	class Input {
+	public:
+        enum class MouseButton {
+            Left,
+            Right,
+            Middle
+        };
+
+        enum class Keyboard {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G,
+            H,
+            I,
+            J,
+            K,
+            L,
+            M,
+            N,
+            O,
+            P,
+            Q,
+            R,
+            S,
+            T,
+            U,
+            V,
+            W,
+            X,
+            Y,
+            Z,
+            SPACE,
+			ENTER,
+            UP,
+			DOWN,
+			LEFT,
+            RIGHT,
+            ESCAPE,
+            SHIFT,
+            CONTROL,
+            ALT,
+			TAB
+        };
+        
+        /**
+        * @brief Creates an Input type object.
+        * @param Window& window Reference to a Window type object.
+        * @details Register a new Input.
+        */
+		static Input inputCreate(Window& window);
+
+        /**
+        * @brief Destructor
+        * @details 
+        */
+		~Input();
+
+		//--Dissable copy constructor and enable noving constructors--
+
+        /**
+        * @brief Copy constructor.
+        * @param Input& right Reference to an Input type object.
+        * @details We set it to delete because there is no need to copy anything.
+        */
+		Input(const Input& right) = delete;
+        /**
+        * @brief Copy assignment.
+        * @param Input& right Reference to an Input type object.
+        * @details We set it to delete because there is no need to copy anything.
+        */
+		Input& operator=(const Input& right) = delete;
+
+        /**
+        * @brief Move constructor.
+        * @param Input&& right Reference to an Input type object.
+        * @details --------------
+        */
+		Input(Input&& right) noexcept;
+		Input& operator=(Input&& right) = default;
+
+		//------------Functions-----------------------
+
+        /**
+        * @brief Calls the setKeyCallback function.
+        * @param Keyboard The engine will check if the 
+        * key matches with user's pressed one.
+        * @details ------------
+        */
+		bool isKeyPressed(Keyboard);
+        bool isKeyDown(Keyboard);
+        bool isKeyUp(Keyboard);
+
+        void generateAsciiMap();
+        /**
+        * @brief Calls the setKeyCallback function.
+        * @param Window& window Reference to a Window type object.
+        * @details It allows the input events to happen.
+        */
+        void setKeyboardCallback(Window& window);
+        void setMouseButtonCallback(Window& window);
+        void getMousePosition(int& x, int& y);
+        bool isMouseButtonPressed(MouseButton button);
+        bool isMouseButtonDown(MouseButton button);
+        void setWindow(Window* window);
+        void clearMouseBuffers();
+		void clearBuffers();
+
+        //< Key pressed by the user.
+        static std::vector<int> pressKey;
+        //< Key being holded by the user.
+        static std::vector<int> repeatKey;
+        //< Key released this frame by the user.
+        static std::vector<int> releaseKey;
+        static std::vector<int> pressMouseButton;
+        static std::vector<int> releaseMouseButton;
+        static std::vector<int> repeatMouseButton;
+
+
+	private:
+        /**
+        * @brief Constructor
+        * @details Clears repeatKey and calls a function
+        * in order to clear all buffers.
+        */
+        Input();
+        //< Map that matches a key with their ascii number
+        std::map<Keyboard, std::vector<int>> asciiMap;
+        Window* window_ = nullptr;
+	};
+}
---
+++ src/win64/Input.cpp
@@ -0,0 +1,211 @@
+#include <Motarda/Input.hpp>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+namespace MTRD {
+    std::vector<int> Input::pressKey = {};
+    std::vector<int> Input::repeatKey = {};
+    std::vector<int> Input::releaseKey = {};
+    std::vector<int> Input::pressMouseButton = {};
+    std::vector<int> Input::releaseMouseButton = {};
+    std::vector<int> Input::repeatMouseButton = {};
+
+    void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+
+        if (action == 1) {
+            Input::repeatKey.push_back(key);
+            Input::pressKey.push_back(key);
+        } else if (action == 0) {
+            Input::repeatKey.erase(std::remove(
+                Input::repeatKey.begin(),
+                Input::repeatKey.end(),
+                key),
+                Input::repeatKey.end());
+
+            Input::releaseKey.push_back(key);
+        }
+    }
+
+    void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+        ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
+
+        if (action == 1) {
+            Input::repeatMouseButton.push_back(button);
+            Input::pressMouseButton.push_back(button);
+        } else if (action == 0) {
+            Input::repeatMouseButton.erase(std::remove(
+                Input::repeatMouseButton.begin(),
+                Input::repeatMouseButton.end(),
+                button),
+                Input::repeatMouseButton.end());
+            Input::releaseMouseButton.push_back(button);
+        }
+    }
+
+    Input Input::inputCreate(Window& window) {
+        Input inp;
+        inp.setKeyboardCallback(window);
+        inp.setMouseButtonCallback(window);
+        return inp;
+    }
+
+    void Input::clearBuffers() {
+        pressKey.clear();
+        releaseKey.clear();
+    }
+
+    void Input::clearMouseBuffers() {
+        pressMouseButton.clear();
+        releaseMouseButton.clear();
+    }
+
+    Input::Input() {
+    }
+
+    void Input::setKeyboardCallback(Window& window) {
+        window.setKeyCallback(keyCallback);
+    }
+
+    void Input::setMouseButtonCallback(Window& window) {
+        window.setMouseButtonCallback(mouseButtonCallback);
+    }
+
+    void Input::setWindow(Window* window) {
+        window_ = window;
+    }
+
+    void Input::getMousePosition(int& x, int& y) {
+        if (window_) {
+            window_->getMousePosition(x, y);
+        }
+    }
+
+    bool checkVector(int numbr, std::vector<int> vector) {
+        for (int key : vector) {
+            if (numbr == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool Input::isKeyPressed(Keyboard key) {
+        if (repeatKey.empty()) return false;
+
+        auto it = asciiMap.find(key);
+        if (it == asciiMap.end()) return false;
+
+        for (int numbr : it->second) {
+            if (checkVector(numbr, repeatKey)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool Input::isKeyDown(Keyboard key) {
+        if (pressKey.empty()) return false;
+
+        auto it = asciiMap.find(key);
+        if (it == asciiMap.end()) return false;
+
+        for (int numbr : it->second) {
+            if (checkVector(numbr, pressKey)) {
+                Input::pressKey.erase(std::remove(
+                    Input::pressKey.begin(),
+                    Input::pressKey.end(),
+                    numbr),
+                    Input::pressKey.end());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool Input::isKeyUp(Keyboard key) {
+        if (releaseKey.empty()) return false;
+
+        auto it = asciiMap.find(key);
+        if (it == asciiMap.end()) return false;
+
+        for (int numbr : it->second) {
+            if (checkVector(numbr, releaseKey)) {
+                Input::releaseKey.erase(std::remove(
+                    Input::releaseKey.begin(),
+                    Input::releaseKey.end(),
+                    numbr),
+                    Input::releaseKey.end());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void Input::generateAsciiMap() {
+        for (int i = 0; i < 26; i++) {
+            asciiMap[static_cast<Keyboard>(i)].push_back(65 + i);
+            asciiMap[static_cast<Keyboard>(i)].push_back(97 + i);
+        }
+
+        asciiMap[Keyboard::SPACE].push_back(GLFW_KEY_SPACE);
+        asciiMap[Keyboard::ENTER].push_back(GLFW_KEY_ENTER);
+        asciiMap[Keyboard::UP].push_back(GLFW_KEY_UP);
+        asciiMap[Keyboard::DOWN].push_back(GLFW_KEY_DOWN);
+        asciiMap[Keyboard::LEFT].push_back(GLFW_KEY_LEFT);
+        asciiMap[Keyboard::RIGHT].push_back(GLFW_KEY_RIGHT);
+        asciiMap[Keyboard::ESCAPE].push_back(GLFW_KEY_ESCAPE);
+        asciiMap[Keyboard::TAB].push_back(GLFW_KEY_TAB);
+
+        asciiMap[Keyboard::SHIFT].push_back(GLFW_KEY_LEFT_SHIFT);
+        asciiMap[Keyboard::SHIFT].push_back(GLFW_KEY_RIGHT_SHIFT);
+
+        asciiMap[Keyboard::CONTROL].push_back(GLFW_KEY_LEFT_CONTROL);
+        asciiMap[Keyboard::CONTROL].push_back(GLFW_KEY_RIGHT_CONTROL);
+
+        asciiMap[Keyboard::ALT].push_back(GLFW_KEY_LEFT_ALT);
+        asciiMap[Keyboard::ALT].push_back(GLFW_KEY_RIGHT_ALT);
+    }
+
+    bool Input::isMouseButtonPressed(MouseButton button) {
+        int btn = -1;
+        switch (button) {
+            case MouseButton::Left: btn = GLFW_MOUSE_BUTTON_LEFT; break;
+            case MouseButton::Right: btn = GLFW_MOUSE_BUTTON_RIGHT; break;
+            case MouseButton::Middle: btn = GLFW_MOUSE_BUTTON_MIDDLE; break;
+        }
+
+        return checkVector(btn, repeatMouseButton);
+    }
+
+    bool Input::isMouseButtonDown(MouseButton button) {
+        int btn = -1;
+        switch (button) {
+            case MouseButton::Left: btn = GLFW_MOUSE_BUTTON_LEFT; break;
+            case MouseButton::Right: btn = GLFW_MOUSE_BUTTON_RIGHT; break;
+            case MouseButton::Middle: btn = GLFW_MOUSE_BUTTON_MIDDLE; break;
+        }
+
+        if (checkVector(btn, pressMouseButton)) {
+            Input::pressMouseButton.erase(std::remove(
+                Input::pressMouseButton.begin(),
+                Input::pressMouseButton.end(),
+                btn),
+                Input::pressMouseButton.end());
+            return true;
+        }
+        return false;
+    }
+
+    Input::~Input() {
+    }
+
+    Input::Input(Input&& right) noexcept :
+        asciiMap{ std::move(right.asciiMap) },
+        window_{ right.window_ } {
+        right.window_ = nullptr;
+    }
+}
